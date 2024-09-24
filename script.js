b97adfdf90f8f02cbef6ebf88f0dcac42e2a398f
Hamdiibrahim <yusufhamdi137@gmail.com>
2024-09-24 20:58:58 +0300

    my-personal-website

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const form = document.getElementById('contactForm');
+const sendMessageButton = document.getElementById('sendMessageButton');
+form.addEventListener ('submit',function(event){
+    event.preventDefault();
+
+const message= docuent.getElementById('Message').value;
+if (message.trim() === ""){
+    alert("Please enter your message.");
+} else {("message.sent: " + message);
+document.getElementById('message').value = '';
+}
+});
